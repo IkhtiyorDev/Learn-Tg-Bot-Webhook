c0a116cbe0dd530fbdb7d2e24f7d8a4b4611dd61
IkhtiyorDev <ikhtiyordev@gmail.com>
2022-02-15 22:14:21 +0500

    first commit

---
+++ src/main/java/dev/ikhtiyor/learntgwebhook/LearnTgWebhookApplication.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.learntgwebhook;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LearnTgWebhookApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(LearnTgWebhookApplication.class, args);
+    }
+
+}
---
+++ src/test/java/dev/ikhtiyor/learntgwebhook/LearnTgWebhookApplicationTests.java
@@ -0,0 +1,13 @@
+package dev.ikhtiyor.learntgwebhook;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LearnTgWebhookApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
